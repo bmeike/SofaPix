10beff98b9286e2df79eb5fb6454c4c7a713ab35
G. Blake Meike <blake.meike@couchbase.com>
2019-01-15 13:23:55 -0800

    Start project

---
+++ app/src/debug/java/com/couchbase/android/sofapix/logging/DebugLogger.kt
@@ -0,0 +1,48 @@
+//
+//
+//
+// Copyright (c) 2019 Couchbase, Inc All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package com.couchbase.android.sofapix.logging
+
+object DebugLogger : Logger {
+    override fun e(tag: String, message: String, ex: Throwable) {
+        android.util.Log.e(tag, message, ex)
+    }
+
+    override fun e(tag: String, message: String) {
+        android.util.Log.e(tag, message)
+    }
+
+    override fun w(tag: String, message: String, ex: Throwable) {
+        android.util.Log.w(tag, message, ex)
+    }
+
+    override fun w(tag: String, message: String) {
+        android.util.Log.w(tag, message)
+    }
+
+    override fun i(tag: String, message: String, ex: Throwable) {
+        android.util.Log.i(tag, message, ex)
+    }
+
+    override fun i(tag: String, message: String) {
+        android.util.Log.i(tag, message)
+    }
+
+    override fun d(tag: String, message: String) {
+        android.util.Log.d(tag, message)
+    }
+}
---
+++ app/src/main/java/com/couchbase/android/sofapix/logging/Logger.kt
@@ -0,0 +1,44 @@
+//
+//
+//
+// Copyright (c) 2019 Couchbase, Inc All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package com.couchbase.android.sofapix.logging
+
+
+var LOG: Logger = NullLogger
+    private set(logger: Logger) { field = logger }
+
+fun SET_LOGGER(logger: Logger) { LOG = logger }
+
+interface Logger {
+    fun e(tag: String, message: String)
+    fun e(tag: String, message: String, ex: Throwable)
+    fun w(tag: String, message: String)
+    fun w(tag: String, message: String, ex: Throwable)
+    fun i(tag: String, message: String)
+    fun i(tag: String, message: String, ex: Throwable)
+    fun d(tag: String, message: String)
+}
+
+object NullLogger : Logger {
+    override fun e(tag: String, message: String) = Unit
+    override fun e(tag: String, message: String, ex: Throwable) = Unit
+    override fun w(tag: String, message: String) = Unit
+    override fun w(tag: String, message: String, ex: Throwable) = Unit
+    override fun i(tag: String, message: String) = Unit
+    override fun i(tag: String, message: String, ex: Throwable) = Unit
+    override fun d(tag: String, message: String) = Unit
+}
